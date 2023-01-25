e8cb504beb61ddca89e26c888da1809720ded1d4
Ranujee123 <117263085+Ranujee123@users.noreply.github.com>
2023-01-25 14:01:10 +0530

    Delete vidhara directory

--- vidhara/script.js
+++
@@ -1,8 +0,0 @@
-let navState = false;
-
-function handleOnMenu() {
-    const nav = document.getElementById('navbarBasicExample');
-    navState = !navState;
-
-    nav.style.display = navState ? 'block' : 'none';
-}
